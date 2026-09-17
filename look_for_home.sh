95a609406744a06d2f0c812cccd4421b5442069b
DASV-C137 <a01644209@tec.mx>
2026-09-17 12:40:03 -0600

    Update look_for_home.sh

--- look_for_home.sh
+++ look_for_home.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Se fija la carpeta por defecto y se pide la palabra al usuario
 read -p "Ingresa la palabra a buscar: " palabra
-
-
 echo -e "\nBuscando '$palabra' en archivos .txt dentro de 'carpeta'..."
 echo "------------------------------------------------------"
-
-# Búsqueda en los archivos .txt de la carpeta
 resultados=$(grep -ril --include="*.txt" "$palabra" carpeta)
-
 echo "$resultados"
